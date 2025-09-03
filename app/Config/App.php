a300b3fd67dccf52803240038a9849012c30bb78
Ilham-Jully-Pratama <ilhamjullypratama30@gmail.com>
2025-09-03 14:36:49 +0700

    revisi

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'https://qabintang7ckr.my.id/';
+    public string $baseURL = 'qabintang7ckr.my.id';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
